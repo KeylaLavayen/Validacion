fa5e42b773caef5f4cf7e3ae7926017a0fdd1929
Keyla <keylalavayenj@hotmail.com>
2024-11-29 18:33:49 -0500

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  contrasena: string = '';
+  confirmarContrasena: string = '';
+  mensaje: string | null = null;
+  colorMensaje: string = 'danger';
 
   constructor() {}
 
+  validarContrasenas() {
+    if (this.contrasena === this.confirmarContrasena) {
+      this.mensaje = '¡Las contraseñas coinciden!';
+      this.colorMensaje = 'success';
+    } else {
+      this.mensaje = 'Las contraseñas no coinciden.';
+      this.colorMensaje = 'danger';
+    }
+  }
 }
+
